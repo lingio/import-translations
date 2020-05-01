9f0a923ab1994f4a6e944fee1d808fb911ef8865
Mika <mika@wappia.se>
2020-05-01 12:01:35 +0800

    Object.freeze

--- stringify.js
+++ stringify.js
@@ -1,10 +1,14 @@
 export default function stringify(obj) {
-  return JSON.stringify(obj, null, 2)
-    .replace(/"([a-zA-Z]+)":/g, "$1:")
-    .replace(/"/g, `\``)
-    .replace(/`([^`]+)`:/g, '"$1":')
-    .replace(
-      /\{\s+text: `([^`]+)`,\s+rtl: (true|false),\s+languageId: `([^`]+)`\s+\},?/gmu,
-      `{ text: \`$1\`, rtl: $2, languageId: \`$3\` },`
-    )
+  return (
+    `Object.freeze(` +
+    JSON.stringify(obj, null, 2)
+      .replace(/"([a-zA-Z]+)":/g, "$1:")
+      .replace(/"/g, `\``)
+      .replace(/`([^`]+)`:/g, '"$1":')
+      .replace(
+        /\{\s+text: `([^`]+)`,\s+rtl: (true|false),\s+languageId: `([^`]+)`\s+\},?/gmu,
+        `Object.freeze({ text: \`$1\`, rtl: $2, languageId: \`$3\` }),`
+      ) +
+    `)`
+  )
 }
