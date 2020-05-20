c3df10ed2a132ac9f3ebb19584f95018efd4520e
Per-Olov Jernberg <possan@possan.se>
2020-05-20 17:33:03 +0200

    Fix quoted strings, maybe

--- stringify.js
+++ stringify.js
@@ -1,14 +1,19 @@
+const processLine = (input) => input
+  .replace(/"([a-zA-Z]+)":/g, `$1:`) // remove quotes on keys
+  .replace(/"(.*)"/g, `\`$1\``) // replace outermost double quotes with backticks
+
 export default function stringify(obj) {
   return (
     `Object.freeze(` +
     JSON.stringify(obj, null, 2)
-      .replace(/"([a-zA-Z]+)":/g, "$1:")
-      .replace(/"/g, `\``)
-      .replace(/`([^`]+)`:/g, '"$1":')
+      .split('\n')
+      .map(r => processLine(r))
+      .join('\n')
       .replace(
         /\{\s+text: `([^`]+)`,\s+rtl: (true|false),\s+languageId: `([^`]+)`\s+\},?/gmu,
         `Object.freeze({ text: \`$1\`, rtl: $2, languageId: \`$3\` }),`
-      ) +
+      ) // flatten keys into one line
+      +
     `)`
   )
 }
